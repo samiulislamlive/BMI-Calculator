98381ac3e6f9fcfa4e83a6d03b0f77fed1f1d0c1
samiulislamlive <samiulislam31live@gmail.com>
2022-01-25 12:49:37 +0600

    "Finally Done"

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,52 @@
+import 'dart:async';
 import 'package:bmicalculator/homepage.dart';
 import 'package:flutter/material.dart';
 
-void main()=> runApp(MyApp());
 
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new MyApp(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    new Future.delayed(
+        const Duration(seconds: 3),
+            () => Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Homepage()),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Homepage(),
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/images/bmi_splash.png',
+              ),
+              fit: BoxFit.cover,
+            ),
+            shape: BoxShape.circle,
+          ),
+        ),
+      ),
     );
   }
 }
-
